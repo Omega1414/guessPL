392f79618ac515235beaf4e04671c42a9737bc72
Babazade <babazadevasif14@gmail.com>
2025-07-19 17:58:46 +0400

    header

--- src/app/header.js
+++ src/app/header.js
@@ -7,7 +7,7 @@ import { db } from "../../firebaseConfig";
 import Link from "next/link";
 import Loading from "@/utils/loading";
 import Image from "next/image";
-import headerImg from "../../public/headIsi.png"
+import headerImg from "../../public/header1.png"
 import "./header.css"
 export default function Header() {
   const [email, setEmail] = useState("");
